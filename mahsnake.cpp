65bc17cc8500f262ffbfcdb94a1ff93346066d8f
edx <edx1010@gmail.com>
2011-01-02 19:13:58 +0100

    Now with background and images, improved move thread

--- mahsnake.cpp
+++ mahsnake.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <utility>
-#include <queue>
 #include <vector>
 #include <cmath>
 #include <SDL/SDL.h>
@@ -8,25 +7,33 @@
 #define PI 3.14159265
 #define ELEM_SIZE 20
 
+#define DEBUG
+
+#ifdef DEBUG
+#define DEBUG_PRINT(X) std::cout << __FILE__ << ' ' << __LINE__ << ' ' << X << std::endl;
+#else
+#define DEBUG_PRINT(X) //
+#endif
+
 using namespace std;
 
-int key; //1 => Left, 2 => Right, 3 => Up, 4 => Down
-int alive;
+short key; //1 => Left, 2 => Right, 3 => Up, 4 => Down
+bool alive;
+SDL_Surface * background = NULL;
 
 class Element;
 
 vector< pair< pair<int, int>, Element* > > snake;
 
-void putpixel(SDL_Surface *, uint16_t, uint16_t, uint8_t, uint8_t, uint8_t);
-void drawcircle(SDL_Surface *, uint16_t, uint16_t, double, int16_t, int16_t, uint8_t, uint8_t, uint8_t);
-void drawrectangle(SDL_Surface *, uint16_t, uint16_t, uint16_t, uint16_t, uint8_t, uint8_t, uint8_t);
+void drawelement(SDL_Surface *, SDL_Surface *, uint16_t, uint16_t, uint16_t, uint16_t);
+void cleanrectangle(SDL_Surface *, uint16_t, uint16_t, uint16_t, uint16_t);
 int move(void *);
 
 class Element
 {
 	public:
-		Element() : pos(0, 0), prev(-1, -1), screen(NULL) {}
-		Element(SDL_Surface * scr, pair<int, int> p) : pos(p), prev(-1, -1), screen(scr) {}
+		Element() : pos(0, 0), prev(-1, -1), screen(NULL), element(NULL) {}
+		Element(SDL_Surface * scr, SDL_Surface * elem, pair<int, int> p) : pos(p), prev(-1, -1), screen(scr), element(elem) {}
 
 		pair<int, int> getPos() const { return this->pos; }
 		void setPos(pair<int, int> p) { pos = p; }
@@ -35,9 +42,9 @@ class Element
 		virtual Element& Move()
 		{
 			if(prev.first != -1)
-				drawcircle(screen, prev.first, prev.second, (ELEM_SIZE / 2), 0, 0, 0x00, 0x00, 0x00);
+				cleanrectangle(screen, prev.first, prev.second, ELEM_SIZE + 1, ELEM_SIZE + 1);
 
-			drawcircle(screen, pos.first, pos.second, (ELEM_SIZE / 2), 0, 0, 0xff, 0x00, 0x00);
+			drawelement(element, screen, pos.first, pos.second, ELEM_SIZE, ELEM_SIZE);
 			prev = pos;
 
 			return *this;
@@ -52,29 +59,15 @@ class Element
 		pair<int, int> pos;
 		pair<int, int> prev;
 		SDL_Surface * screen;
-};
-
-class Head : public Element
-{
-	public:
-		Head() : Element() {}
-		Head(SDL_Surface * scr, pair<int, int> p) : Element(scr, p)  {}
-
-		virtual Element& Move()
-		{
-			if(prev.first != -1)
-				drawrectangle(screen, prev.first, prev.second, ELEM_SIZE, ELEM_SIZE, 0x00, 0x00, 0x00);
-
-			drawrectangle(screen, pos.first, pos.second, ELEM_SIZE, ELEM_SIZE, 0xff, 0x00, 0x00);
-			prev = pos;
-
-			return *this;
-		}
+		SDL_Surface * element;
 };
 
 int main()
 {
 	SDL_Surface * screen = NULL;
+	SDL_Surface * background_tmp = NULL;
+	SDL_Surface * elem_tmp = NULL;
+	SDL_Surface * elem = NULL;
 	SDL_Event e;
 	SDL_Thread * move_t = NULL;
 	int pressed = 0;
@@ -94,15 +87,38 @@ int main()
 		exit(1);
 	}
 
-	snake.push_back(pair< pair<int, int>, Element* >(pair<int, int>(-1, -1), new Head(screen, pair<int, int>(200, 50))));
-	for(int i = 180; i > 0; i -= 20)
-		snake.push_back(pair< pair<int, int>, Element* >(pair<int, int>(i + 20, 50), new Element(screen, pair<int, int>(i, 50))));
+	SDL_WM_SetCaption("Mahsnake", NULL);
+
+	background_tmp = SDL_LoadBMP("bkg.bmp");
+	if(background_tmp == NULL){
+		cout << "Can't load background" << endl;
+		exit(1);
+	}
+
+	background = SDL_DisplayFormat(background_tmp);
+	SDL_FreeSurface(background_tmp);
+
+	SDL_BlitSurface(background, NULL, screen, NULL);
+	SDL_Flip(screen);
+
+	elem_tmp = SDL_LoadBMP("snake.bmp");
+	if(elem_tmp == NULL){
+		cout << "Can't load element bitmap" << endl;
+		exit(1);
+	}
+
+	elem = SDL_DisplayFormat(elem_tmp);
+	SDL_FreeSurface(elem_tmp);
+
+	snake.push_back(pair< pair<int, int>, Element* >(pair<int, int>(-1, -1), new Element(screen, elem, pair<int, int>(120, 50))));
+	for(int i = 100; i > 0; i -= 20)
+		snake.push_back(pair< pair<int, int>, Element* >(pair<int, int>(i + 20, 50), new Element(screen, elem, pair<int, int>(i, 50))));
 
 	key = 2;
-	alive = 1;
+	alive = true;
 	move_t = SDL_CreateThread(move, static_cast<void *>(screen));
 
-	while((!pressed) || alive)
+	while((!pressed) && alive)
 	{
         	while(SDL_PollEvent(&e))
         	{
@@ -142,6 +158,9 @@ int main()
     	}
 
 	SDL_WaitThread(move_t, NULL);
+	SDL_FreeSurface(elem);
+	SDL_FreeSurface(background);
+	SDL_FreeSurface(screen);
 
 	for(int i = 0; i < snake.size(); i++)
 		delete(snake[i].second);
@@ -153,7 +172,7 @@ int move(void * p)
 {
 	SDL_Surface * screen = static_cast<SDL_Surface *>(p);
 
-	cout << "Move thread starting" << endl;
+	DEBUG_PRINT("Move thread starting");
 
 	while(alive)
 	{
@@ -174,6 +193,11 @@ int move(void * p)
 
 					snake[0].second->setPos(make_pair(coord.first - ELEM_SIZE, coord.second));
 				}
+				else
+				{
+					cout << "You lost" << endl;
+					alive = false;
+				}
 			break;
 
 			case 2:
@@ -190,6 +214,11 @@ int move(void * p)
 					}
 
 					snake[0].second->setPos(make_pair(coord.first + ELEM_SIZE, coord.second));
+				}
+				else
+				{
+					cout << "You lost" << endl;
+					alive = false;
 				}
 			break;
 
@@ -208,6 +237,11 @@ int move(void * p)
 
 					snake[0].second->setPos(make_pair(coord.first, coord.second - ELEM_SIZE));
 				}
+				else
+				{
+					cout << "You lost" << endl;
+					alive = false;
+				}
 			break;
 
 			case 4:
@@ -225,76 +259,45 @@ int move(void * p)
 
 					snake[0].second->setPos(make_pair(coord.first, coord.second + ELEM_SIZE));
 				}
+				else
+				{
+					cout << "You lost" << endl;
+					alive = false;
+				}
 			break;
 		}
 
-		SDL_Delay(75);
+		SDL_Delay(50);
 	}
-	alive = 0;
+	alive = false;
 
-	cout << "Move thread ending" << endl;
+	DEBUG_PRINT("Move thread ending");
 
 	return 0;
 }
 
-void putpixel(SDL_Surface * screen, uint16_t x, uint16_t y, uint8_t R, uint8_t G, uint8_t B)
+void drawelement(SDL_Surface * in, SDL_Surface * out, uint16_t x, uint16_t y, uint16_t base, uint16_t height)
 {
-    uint16_t * ptr = NULL;
- 
-    if((x <= 0) || (y <= 0))
-        return;
- 
-    if((x >= screen->w) || (y >= screen->h))
-        return;
- 
-    ptr = (uint16_t *)screen->pixels + y*screen->pitch/2 + x;
-    *ptr = SDL_MapRGB(screen->format, R, G, B);
+	SDL_Rect rect;
+
+	rect.x = x - (base / 2);
+    	rect.y = y - (height / 2);
+    	rect.w = base;
+    	rect.h = height;
+
+	SDL_BlitSurface(in, NULL, out, &rect);
+	SDL_Flip(out);
 }
 
-void drawcircle(SDL_Surface * screen, uint16_t x, uint16_t y, double radius, int16_t a, int16_t b, uint8_t R, uint8_t G, uint8_t B)
+void cleanrectangle(SDL_Surface * screen, uint16_t x, uint16_t y, uint16_t base, uint16_t height)
 {
-    double i;
-    SDL_Rect rect;
-    int16_t xX, yY = 0;
- 
-    rect.x = x - radius - (b - a);
-    rect.y = y - radius - (a - b);
-    rect.w = (radius * 2) + ((b - a) * 2);
-    rect.h = (radius * 2) + ((a - b) * 2);
- 
-    for(i = 0; i < 360; i+=0.1)
-    {
-        xX = x + ((radius + (b - a)) * cos(i * PI/180));
-        yY = y + ((radius + (a - b)) * sin(i * PI/180));
- 
-        putpixel(screen, xX, yY, R, G, B);
-    }
- 
-    SDL_UpdateRects(screen, 1, &rect);
-}
+	SDL_Rect rect;
 
-void drawrectangle(SDL_Surface * screen, uint16_t x, uint16_t y, uint16_t base, uint16_t height, uint8_t R, uint8_t G, uint8_t B)
-{
-    int i;
-    SDL_Rect rect;
- 
-    rect.x = x - (base / 2);
-    rect.y = y - (height / 2);
-    rect.w = base;
-    rect.h = height;
- 
-    for(i = 0; i < base; i++)
-        putpixel(screen, (rect.x + i), rect.y, R, G, B);
- 
-    for(i = 0; i < base; i++)
-        putpixel(screen, (rect.x + i), (rect.y + height - 1), R, G, B);
- 
-    for(i = 0; i < height; i++)
-        putpixel(screen, rect.x, (rect.y + i), R, G, B);
- 
-    for(i = 0; i < height; i++)
-        putpixel(screen, (rect.x + base - 1), (rect.y + i), R, G, B);
- 
- 
-    SDL_UpdateRects(screen, 1, &rect);
+	rect.x = x - (base / 2);
+    	rect.y = y - (height / 2);
+    	rect.w = base;
+    	rect.h = height;
+
+	SDL_BlitSurface(background, &rect, screen, &rect);
+	SDL_Flip(screen);
 }
